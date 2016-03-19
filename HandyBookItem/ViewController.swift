20c1e667249598af89245260d27a5cf45d6e6859
Charles Gong <cggong@uchicago.edu>
2016-03-19 11:39:37 -0500

    Initial Commit

---
+++ HandyBookItem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandyBookItem
+//
+//  Created by 宫澄光 on 3/19/16.
+//  Copyright © 2016 cggong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
